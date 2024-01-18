6cee4ea49491e5202c0ea953197155fe6c949213
Davey <david.c.hays.iii@gmail.com>
2024-01-18 04:14:20 +0000

    small update

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-char str(30);
-strcpy(str, "Programming is like building a multilingual puzzle");
-puts(str);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
