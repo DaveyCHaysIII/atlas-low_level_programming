cbea1f2755cea226c4509232164c92c49e14092e
Davey <david.c.hays.iii@gmail.com>
2024-01-20 22:00:46 +0000

    added file 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, using _putchar only
+ *
+ * Return: void ()
+ */
+
+void print_alphabet(void)
+{
+char  i;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('/n');
+}
+
