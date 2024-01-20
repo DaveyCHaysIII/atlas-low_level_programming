9e3f71b85c4b0d2f55c8dc4f3816265a535dffa9
Davey <david.c.hays.iii@gmail.com>
2024-01-20 22:26:43 +0000

    small update

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 {
 int j;
 for (j = 0; j < 10; j++)
-        {
-        char  i;
+	{
+	char  i;
 
-        for (i = 'a'; i <= 'z'; i++)
-                {
-                putchar(i);
-                }
-                putchar('\n');
-        }
+	for (i = 'a'; i <= 'z'; i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	}
 }
