b6054d7782a4f075ed24c4657b44656e9d9aa924
Davey <david.c.hays.iii@gmail.com>
2024-01-20 22:21:27 +0000

    added file 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - runs print_alphabet 10x times
+ *
+ * Return: void ()
+ */
+
+void print_alphabet_x10(void)
+{
+int j;
+for (j = 0; j < 10; j++)
+        {
+        char  i;
+
+        for (i = 'a'; i <= 'z'; i++)
+                {
+                putchar(i);
+                }
+                putchar('/n');
+        }
+}
