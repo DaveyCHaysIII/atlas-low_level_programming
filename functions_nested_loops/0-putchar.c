958fa0bb795463b1e87a6cd0740ea853828ef942
Davey <david.c.hays.iii@gmail.com>
2024-01-20 21:10:49 +0000

    small update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,14 @@
  */
 int main(void)
 {
-_putchar();
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
