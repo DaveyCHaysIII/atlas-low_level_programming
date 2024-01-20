0cf1078b1aeaee9c0400e7698b08ce6f37529998
Davey <david.c.hays.iii@gmail.com>
2024-01-20 22:40:47 +0000

    added file 3

---
+++ functions_nested_loops/3-is_lower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_lower - checks to see if a given value is lowercase
+ *
+ * Return: 0 if c is lowercase, 1 if not
+ */
+
+int is_lower(int c)
+{
+if( 97 <= c <= 122)
+{
+return (0);
+}
+else
+{
+return (1);
+}
+}
