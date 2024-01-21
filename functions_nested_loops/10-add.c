673257e824f292d2ca81f165a1525e8a9cf92135
Davey <david.c.hays.iii@gmail.com>
2024-01-21 18:48:48 +0000

    added file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - sums two numbers
+ *
+ * @x: first number
+ *
+ * @y: second number
+ *
+ * Return: sum of x and y
+ *
+ */
+
+int add(int x, int y)
+{
+int sum;
+sum = (x + y);
+return (sum);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,5 +13,7 @@ int _abs(int n);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
+
 
 #endif
