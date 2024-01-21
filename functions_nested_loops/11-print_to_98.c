c63960d1033bc797365f4966564d33a85df2fe02
Davey <david.c.hays.iii@gmail.com>
2024-01-21 19:13:44 +0000

    added file 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ * @n: the number away from 98
+ *
+ */
+
+void print_to_98(int n)
+{
+if (n < 98)
+{
+n = 98 - n;
+while (n < 99)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+else
+{
+printf("%d", n);
+}
+n++;
+}
+}
+else if (n > 98)
+{
+n = n - 98;
+while (n > 97)
+{
+if (n != 98)
+{
+printf("%d, ", n);
+}
+else
+{
+printf("%d", n);
+}
+n--;
+}
+}
+}
+
