ece4c22b7626500fc43028ab25344e62131c6bbe
Davey <david.c.hays.iii@gmail.com>
2024-01-25 03:13:06 +0000

    added file 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul()- multiplies a and b, returns product
+ *
+ * @a: the digit in question
+ * @b: the digit in question
+ *
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
