bacc971b013c34144073d44802ef9e6b485ce2a9
Davey <david.c.hays.iii@gmail.com>
2024-01-25 03:22:03 +0000

    added file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers()- prints numbers to terminal
+ *
+ * Return: no return
+ *
+ */
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+}
