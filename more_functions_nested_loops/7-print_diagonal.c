345e044010b5221e308b038cb6fe4acd4584fbfb
Davey <david.c.hays.iii@gmail.com>
2024-01-25 04:55:58 +0000

    small update

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,18 +11,24 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		int i;
+	int i, j;
 
-		for (i = 1; i <= n; i++)
+	for (i = 0; i <= n; i++)
 		{
+		j = i;
+
+		while (j > 0)
+			{
 			_putchar(' ');
-		}
+			j--;
+			}
 		_putchar('\\');
 		_putchar('\n');
+		}
 	}
 }
 
