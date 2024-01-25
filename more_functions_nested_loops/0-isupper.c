6251def7e4f0a2ca907ddf6436c45a13f7fd680a
Davey <david.c.hays.iii@gmail.com>
2024-01-25 02:49:09 +0000

    Added file 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * _isupper(int c)- returns a value if a given int represents an upper case ASCII character or not
+ *
+ *@c: The character in question
+ *
+ * Return: 1 if c is uppercase, else 0
+ *
+ */
+
+int _isupper(int c)
+{
+if ((c > '40') && (c < '91')
+	return (1);
+	else
+	return (0);
+}
