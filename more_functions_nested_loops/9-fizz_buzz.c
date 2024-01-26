0a989aa16b889924709c0c45a084aff392d9dd17
Davey <david.c.hays.iii@gmail.com>
2024-01-26 03:10:10 +0000

    small update

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,11 @@ int i;
 
 for (i = 1; i < 101; i++)
 	{
-	if ((i % 3 == 0) && (i % 5 == 0))
+	if (i == 100)
+	{
+	printf("Buzz");
+	}
+	else if ((i % 3 == 0) && (i % 5 == 0))
 	{
 	printf("FizzBuzz ");
 	}
@@ -23,10 +27,6 @@ for (i = 1; i < 101; i++)
 	else if (i % 5 == 0)
 	{
 	printf("Buzz ");
-	}
-	else if (i == 100)
-	{
-	printf("Buzz");
 	}
 	else
 	{
