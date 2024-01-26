9fbb0a34dcb4d940ef3998b04433c072b6e4c4a6
Davey <david.c.hays.iii@gmail.com>
2024-01-26 03:38:45 +0000

    setup next folder/files

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98()- resets the variable at location n to 98
+ * @*n: pointer in question
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+
+
+}
