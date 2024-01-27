eb771e9f75394b01f55e265b3fca98b0dba0484b
Davey <david.c.hays.iii@gmail.com>
2024-01-27 23:50:37 +0000

    added file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts()- prints a string
+ * @str: the string in question
+ *
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+int i;
+
+i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
