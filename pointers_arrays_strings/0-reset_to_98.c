11118c57280785f0d6278b92a21fce6458697d3b
Davey <david.c.hays.iii@gmail.com>
2024-01-27 22:45:42 +0000

    small update

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98()- resets the variable at location n to 98
- * @*n: pointer in question
+ * @n: pointer in question
  *
  * Return: no return
  */
@@ -10,4 +10,5 @@
 void reset_to_98(int *n)
 {
 *n = 98;
+
 }
