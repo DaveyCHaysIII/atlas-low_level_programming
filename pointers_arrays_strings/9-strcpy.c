ff6e8b405afbe4d1262f4a05cda0897a1cc299bb
Davey <david.c.hays.iii@gmail.com>
2024-01-28 04:08:43 +0000

    done for the night

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy()- copies a string
+ * @dest: the destination
+ * @src: the string in question
+ *
+ * Return: returns pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+}
