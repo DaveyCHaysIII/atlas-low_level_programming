d6d7e7493b9a3b4d6963345177c74c1812d7e266
Davey <david.c.hays.iii@gmail.com>
2024-01-28 02:46:35 +0000

    added file 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half()- prints the front half of a string if even, back if odd
+ * @str: the string in question
+ *
+ * Return: no return
+ */
+
+void puts_half(char *str)
+{
+int i, j;
+
+i = 0;
+while (str[i] != '0')
+{
+i++;
+}
+j = i / 2;
+if (i % 2)
+{
+for (i = i; i >= j, i--)
+{
+_putchar(str[i];
+}
+}
+else
+{
+for (i = 0; i <= j, i++)
+{
+_putchar(str[i];
+}
+}
+_putchar('\n');
+}
