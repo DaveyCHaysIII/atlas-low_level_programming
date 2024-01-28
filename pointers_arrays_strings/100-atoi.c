00dfa937661b4bc4e0b9edca7ab81453578760ac
Davey <david.c.hays.iii@gmail.com>
2024-01-28 20:26:31 +0000

    added file 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _atoi()- converts a string to an int!
+ * @s: the string in question
+ *
+ * Return: return the converted int
+ */
+
+int _atoi(char *s)
+{
+int i, sign, result;
+
+result = 0;
+sign = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (isdigit(s[i]))
+{
+result = result * 10 + (s[i] - '0');
+}
+else if (s[i] == '-')
+{
+sign--;
+}
+else if (s[i] == '+')
+{
+sign++;
+}
+}
+if (sign < 0)
+{
+return (result * -1);
+}
+else
+{
+return (result);
+}
+}
