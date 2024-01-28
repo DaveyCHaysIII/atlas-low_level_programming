60b62c00836bc191b3d6242f8b9db4e7ee982adc
Davey <david.c.hays.iii@gmail.com>
2024-01-28 03:55:02 +0000

    small update

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array()- prints a given number of integers of an array
+ * @a: the array in question
+ * @n: the number of integers
+ *
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+
+n--;
+for (i = 0; i < n; i++)
+{
+printf("%d, ", a[i]);
+}
+printf("%d\n", a[n]);
+}
