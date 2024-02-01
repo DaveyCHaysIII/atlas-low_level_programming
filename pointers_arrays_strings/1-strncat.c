a4e6fcfcf82025df77e8189a845b23f54cf87ad1
Davey <david.c.hays.iii@gmail.com>
2024-02-01 02:32:16 +0000

    added file 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strncat()- concatenates two strings up to n characters
+ * @dest: the main string
+ * @src: the string to pull from
+ * @n: the number of digits to pull
+ *
+ * Return: a pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, lensrc, j, charcount, usenum;
+
+	i = 0;
+	j = 0;
+	lensrc = 0;
+	charcount = n / 4;
+	charcount = (charcount < 1) ? 1 : charcount;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[lensrc] != '\0')
+	{
+		lensrc++;
+	}
+	if (lensrc > charcount)
+	{
+		usenum = charcount;
+	}
+	else
+	{
+		usenum = lensrc;
+	}
+	while (j != usenum)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
