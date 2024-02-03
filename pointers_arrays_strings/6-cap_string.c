6503e69d46af033f78de42f9eda107f755b20f8f
Davey <david.c.hays.iii@gmail.com>
2024-02-03 05:14:26 +0000

    small update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,8 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+	int arr[] = {10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -18,9 +19,15 @@ char *cap_string(char *s)
 		{
 			s[i] = s[i] - 32;
 		}
-		if ((islower(s[i])) && (!isalnum(s[(i - 1)])))
+		if (islower(s[i]))
 		{
-			s[i] = s[i] - 32;
+			for (j = 0; j < 11; j++)
+			{
+				if (s[(i - 1)] == arr[j])
+				{
+					s[i] = s[i] - 32;
+				}
+			}
 		}
 	}
 	return (s);
