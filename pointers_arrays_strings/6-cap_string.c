eaff09db712d0544f1b93ceb510fbc72c9d17d20
Davey <david.c.hays.iii@gmail.com>
2024-02-03 04:44:31 +0000

    added file 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string()- capitalize all words in string
+ * @s: the string in question
+ *
+ * Return: string
+ */
+
+char *cap_string(char *s);
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((i == 0) && (islowerletter(s[i]) < 0))
+		{
+			s[i] = s[i] - 32;
+		}
+		if ((islowerletter(s[i]) < 0) && !((s[(i - 1)] > 96 && s[(i - 1)] < 123) || ((s[(i -1)] > 64) && (s[(i - 1)] < 91))))
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
+
+/**
+ * islowerletter()- checks if c is a lower case letter
+ * @c: the letter in question
+ *
+ */
+
+int islowerletter(char *c)
+{
+	if ((c > 96) && (c < 123))
+	{
+		return (1);
+	}
+	return (0);
+}
