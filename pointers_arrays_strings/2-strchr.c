a1baa19191614a39dd79d4bb31f774e03f108f54
Davey <david.c.hays.iii@gmail.com>
2024-02-03 19:08:42 +0000

    added file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr()- finds a character in a string
+ * @s: the string in question
+ * @c: the char in question
+ *
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return (NULL);
+}
