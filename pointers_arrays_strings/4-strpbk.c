fbc77d01d430a23e03a990c022480d27783ec786
Davey <david.c.hays.iii@gmail.com>
2024-02-03 20:29:22 +0000

    added file 4

---
+++ pointers_arrays_strings/4-strpbk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk()- returns a pointer to a matching char
+ * @s: the string in question
+ * @accept: the characters looking to match
+ *
+ * Return: pointer to a char
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (NULL);
+}
