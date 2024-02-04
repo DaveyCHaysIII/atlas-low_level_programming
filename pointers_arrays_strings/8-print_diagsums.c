2daa2ed440388f5b806dfa38883f13c855fda215
Davey <david.c.hays.iii@gmail.com>
2024-02-04 03:26:58 +0000

    small update

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,12 +11,14 @@
 void print_diagsums(int *a, int size)
 {
 	int n, i, suma, sumb;
-
-	n = size;
-	for (i = 0; size >= 0; i++)
+	
+	suma = 0;
+	sumb = 0;
+	n = size - 1;
+	for (i = 0; n >= 0; i++)
 	{
-		suma = suma + *a[n][n];
-		sumb = sumb + *a[n][i];
+		suma = suma + a[n][n];
+		sumb = sumb + a[n][i];
 		n--;
 	}
 	printf("%d, %d", suma, sumb);
