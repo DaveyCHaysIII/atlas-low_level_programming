916f7df6136a4ed3d17ce0a6d40be1f3a35d2b32
Davey <david.c.hays.iii@gmail.com>
2024-02-04 21:30:06 +0000

    fixed it

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,12 +11,12 @@ char *leet(char *s)
 {
 	int i, j;
 	char input[] = "aAeEoOtTlL";
-	char output[] = "114433007711";
+	char output[] = "4433007711";
 
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 12; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == input[j])
 			{
