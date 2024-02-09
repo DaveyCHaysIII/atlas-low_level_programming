3b43d524570b450829989d91df8c52e57366f1b8
Davey <david.c.hays.iii@gmail.com>
2024-02-09 05:04:30 +0000

    added file 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion()- calcs the power y of x
+ * @x: the number to multiply
+ * @y: the exponent
+ *
+ * Return: x to the y power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1)
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * (_pow_recursion(x, y - 1)); 
+	}
+
+}
