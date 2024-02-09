d127546d5df5afc8a18bbc4d77c7a4c79f53b757
Davey <david.c.hays.iii@gmail.com>
2024-02-09 02:40:33 +0000

    added file 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion()- recursively puts
+ * @s: the string in question
+ *
+ * Return: no return
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s[0]);
+	if (s[0] != '\0')
+	{
+		_putchar(_puts_recursion(*s + 1));
+	}
+}
