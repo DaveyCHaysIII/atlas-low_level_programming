6d43b5c34d3bd543b21faacfc094d52ed8cd622e
Davey <david.c.hays.iii@gmail.com>
2024-02-10 19:24:06 +0000

    added file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main()- prints the name of the file
+ * @argc: the number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
