89d56ebd3df0e85765cd36ba0412bad78b2ce5de
Davey <david.c.hays.iii@gmail.com>
2024-02-10 19:35:31 +0000

    small update

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main()- prints number of args
+ * @argc: number of args
+ * @argv: args
+ *
+ * Return: 0
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
