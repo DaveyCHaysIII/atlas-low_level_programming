63f94caa7d266cb583f43e2e1a644017e8d9e1fc
Davey <david.c.hays.iii@gmail.com>
2024-02-10 19:42:03 +0000

    added file 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main()- prints out all args
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			printf("%s\n", argv[i]);
+			i++;
+		}
+	}
+	return (0);
+}
