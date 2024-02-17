6a9d9ec2867f624afb9aedb66386e6a1163c82d0
Davey <david.c.hays.iii@gmail.com>
2024-02-17 23:51:50 +0000

    added file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,6 +3,28 @@
 
 int **alloc_grid(int width, int height)
 {
+	int i;
+	int **grid;
 
-	
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int **grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int)calloc(sizeof(int) * width);
+
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
 }
