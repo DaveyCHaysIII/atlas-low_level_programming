316e36978f207b7da5fdddbd28f9669499a62505
Davey <david.c.hays.iii@gmail.com>
2024-02-18 03:09:52 +0000

    small update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,11 +26,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; (s2[j] != '\0') && ((i + j) <= size); j++)
+	for (j = 0; s2[j] != '\0' && (i + j) < (size - 1); j++)
 	{
-		str[i] = s2[j];
-		i++;
+		str[i + j] = s2[j];
 	}
-	str[i + 1] = '\0';
+	str[size - 1] = '\0';
 	return (str);
 }
