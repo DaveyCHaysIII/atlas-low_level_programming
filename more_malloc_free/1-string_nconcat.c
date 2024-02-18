7bb25222232f9080d46c2bac6d0a384f24777055
Davey <david.c.hays.iii@gmail.com>
2024-02-18 02:12:14 +0000

    small update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	str = malloc(sizeof(s1 + n));
+	str = malloc(sizeof(s1) + n);
 	if (str == NULL)
 	{
 		return (NULL);
