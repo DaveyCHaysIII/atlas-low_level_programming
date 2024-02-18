91b9244075ef0178fd69b64fe481a58733dc15c1
Davey <david.c.hays.iii@gmail.com>
2024-02-18 01:43:33 +0000

    added file 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat()-
+ * @s1: the string in question
+ * @s2: the string in question
+ * @n: the number of bytes
+ *
+ * Return: pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+}
