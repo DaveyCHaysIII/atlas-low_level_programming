3290586e466aba2c965a0a7476d4363372dc6a1f
Davey <david.c.hays.iii@gmail.com>
2024-02-18 01:06:25 +0000

    added file 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked()- allocates memory
+ * @b: the number of bytes in question
+ *
+ * Return: no return
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+	{
+		exit(98);
+	}
+}
