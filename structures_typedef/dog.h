cc32b282d65ce84016fb9ea6c621aa5a60054f63
Davey <david.c.hays.iii@gmail.com>
2024-02-22 03:46:10 +0000

    small update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,20 +9,14 @@
  * * Description: its a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}; dog_t
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog {
-	char *name;
-	float age;
-	char *owner
-}; dog_t
 
 #endif
