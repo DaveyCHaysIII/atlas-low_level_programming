26c0f71dc45b14d33c5ce17876f5bf13f63273eb
Davey <david.c.hays.iii@gmail.com>
2024-02-22 03:30:31 +0000

    added file 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,46 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_dog()- prints a dog
+ * @d: the dog that needs a printin
+ *
+ * Return: no return
+ */
+
+void print_dog(struct dog *d)
+{
+	char nil[];
+
+	nil = "nil";
+	if (d != NULL)
+	{
+		if (d.name != NULL)
+		{
+			printf("Name: %s\n", d.name);
+		}
+		else
+		{
+			printf("Name: %s\n", nil);
+		}
+
+		if (d.age != NULL)
+		{
+			printf("Age: %f\n", d.age);
+		}
+		else
+		{
+			printf("Age: %s\n", nil);
+		}
+
+		if (d.owner != NULL)
+		{
+			printf("Owner: %s\n", d.owner);
+		}
+		else
+		{
+			printf("Owner: %s\n", nil);
+		}
+	}
+}
