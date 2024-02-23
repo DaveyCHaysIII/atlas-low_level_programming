47589909fb9b22f704c0a19abbc3ffc956488a04
Davey <david.c.hays.iii@gmail.com>
2024-02-23 02:42:48 +0000

    small update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,10 +16,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *new_name, *new_owner;
 
+	char new_name = malloc(strlen(name) * sizeof(char) + 1);
+
+	if (new_name == NULL)
+	{
+		return (NULL);
+	}
+	char new_owner = malloc(strlen(owner) * sizeof(char) + 1);
+
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		return (NULL);
+	}
+	strcpy(new_name, name);
+	strcpy(new_owner, owner);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
+		free(new_name);
+		free(new_owner);
 		return (NULL);
 	}
 	new_dog->name = name;
