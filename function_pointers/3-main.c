2d3d7e13c39f2fde070b8e7789d31cdccbf396f6
Davey <david.c.hays.iii@gmail.com>
2024-02-24 23:04:40 +0000

    small update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	op = argv[2][0];
 
-	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%' && sizeof(op) > 1)
+	if ((op != '+' && op != '-' && op != '*' && op != '/' && op != '%') || sizeof(op) != 1)
 	{
 		printf("Error\n");
 		return (99);
