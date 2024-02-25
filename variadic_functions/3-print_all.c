ef26b3a16f09c12a82e52392f070954e2adbf30c
Davey <david.c.hays.iii@gmail.com>
2024-02-25 05:29:50 +0000

    small update

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -22,7 +22,7 @@ void print_all(const char * const format, ...)
 
 	va_start(ptr, format);
 	i = 0;
-	while (format[i] != '\0' || format != NULL)
+	while (format[i] != '\0' && format != NULL)
 	{
 		val = 0;
 		switch (format[i])
