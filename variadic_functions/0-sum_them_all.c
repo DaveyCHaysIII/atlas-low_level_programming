eed88eabde4f5535527586e7377fe1ef8a1c2157
Davey <david.c.hays.iii@gmail.com>
2024-02-25 02:42:00 +0000

    added file 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdargs.h>
+
+/**
+ * sum_them_all()- sums all args
+ * @n: number of args
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ptr;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(ptr, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int)
+	}
+	va_end(ptr);
+	return (sum);
+}
