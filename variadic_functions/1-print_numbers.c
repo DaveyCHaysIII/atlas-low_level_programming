f23c7b3a541a26ad48ab5b7907f895bf09fb9a19
Davey <david.c.hays.iii@gmail.com>
2024-02-25 03:15:37 +0000

    small update

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -16,9 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptr;
 
 	va_start(ptr, n);
-	printf("%d", va_arg(ptr, int));
 	if (n != 0)
 	{
+		printf("%d", va_arg(ptr, int));
 		for (i = 1; i < n; i++)
 		{
 			if (separator != NULL)
