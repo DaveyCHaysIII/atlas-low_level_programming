6d70dc687c4c6e8a4564279c444bb8fc89732b16
Davey <david.c.hays.iii@gmail.com>
2024-03-07 03:46:13 +0000

    small update

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,12 +16,15 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new_node == NULL)
 	{
+		free(new_node);
 		return (NULL);
 	}
 
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
+		free(new_node->str);
+		free(new_node);
 		return (NULL);
 	}
 	new_node->len = _strlen(new_node->str);
