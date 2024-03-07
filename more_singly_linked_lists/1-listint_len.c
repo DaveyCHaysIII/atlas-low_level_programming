623ea98b8c65a7aae1e8af878c8eaecc62b2e7e5
Davey <david.c.hays.iii@gmail.com>
2024-03-07 04:46:57 +0000

    added file 1

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * listint_len()- gives number of elements in list
+ * @h: the head of the list
+ *
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t length;
+	const list_t *current = h;
+
+	length = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		length++;
+	}
+	return (length);
+}
+
