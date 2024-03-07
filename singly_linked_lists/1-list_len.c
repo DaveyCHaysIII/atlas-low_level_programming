00c560531269aa3fd1de4d4aac4e41079a54fbcb
Davey <david.c.hays.iii@gmail.com>
2024-03-07 03:07:00 +0000

    added file 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * list_len()- gives number of elements in list
+ * @h: the head of list
+ *
+ * Return: number of elements 
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t length;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		current = current->next;
+		length++;
+	}
+	return (length);
+}
