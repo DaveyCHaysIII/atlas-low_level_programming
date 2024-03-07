29a21ea3bed80b4a0902424785a71490dce32e7e
Davey <david.c.hays.iii@gmail.com>
2024-03-07 02:50:49 +0000

    added file 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list()- prints all elements of a list
+ * @h: the head of the list in queston
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodeCount;
+	char *string;
+	int length;
+	const list_t *current = h;
+
+	nodeCount = 0;
+	while (current->next != NULL)
+	{
+		if (current->str == NULL)
+		{
+			string = "(nil)";
+			length = 0;
+		}
+		else
+		{
+			string = current->str;
+			length = current->len;
+		}
+		printf("[%d] %s", length, string);
+		current = current->next;
+		nodeCount++;
+	}
+	return (nodeCount);
+}
