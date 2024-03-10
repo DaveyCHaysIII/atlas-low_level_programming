edcbb66a8c9b1a1281e11f57da5ca0002780e7be
Davey <david.c.hays.iii@gmail.com>
2024-03-10 20:06:54 +0000

    small update

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -40,9 +40,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			next = current->next;
 		}
 	}
-	*new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 	new->n = n;
