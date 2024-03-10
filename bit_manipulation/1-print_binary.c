88045a1119a34235a7a4af41ebb725eade63418d
Davey <david.c.hays.iii@gmail.com>
2024-03-10 23:01:37 +0000

    stopping point

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_binary()- prints a number to binary string
+ * @n: the number in question
+ *
+ * Return: no return
+ */
+
+void print_binary(unsigned long int n)
+{
+
+	if (n > (1 >> j))
+		j++;
+
+}
