3963b99e8a8cb57ea066abd9b5bdbc61332d9f08
Davey <david.c.hays.iii@gmail.com>
2024-03-10 18:24:25 +0000

    added file 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2()- frees a linked list
+ * @head: the head of the list to free
+ *
+ * Return: no return
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	free(head);
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
