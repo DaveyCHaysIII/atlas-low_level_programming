0329c2934399e3b7876e71cbf486fc6669ecdfe8
Davey <david.c.hays.iii@gmail.com>
2024-03-10 19:33:01 +0000

    added file 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index()- get node at index
+ * @head: top of linked list
+ * @index: the node in question
+ *
+ * Return: the node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	int ll_ind = 0;
+
+	current = head;
+	while (ll_ind < index)
+	{
+		current = current->next;
+		ll_ind++;
+	}
+	return (current);
+}
