25c46d21ee28b7aa628f8008bb68b359e3ad2617
Davey <david.c.hays.iii@gmail.com>
2024-03-10 19:03:43 +0000

    small update

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -12,14 +12,10 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current = *head;
-	listint_t *next;
-
-	if (current == NULL)
-		return;
+	listint_t *next = current->next;
 
 	while (current != NULL)
 	{
-		next = current->next;
 		free(current);
 		current = next;
 	}
