4d910575e6e373006673d78daa4de408d972f9d4
Davey <david.c.hays.iii@gmail.com>
2024-03-10 20:20:21 +0000

    small update

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,31 +13,23 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *current;
-	listint_t *next;
 	listint_t *new;
 	unsigned int curr_idx;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
 
 	curr_idx = 0;
 	current = *head;
-	while (curr_idx < (idx - 1))
+	while (current != NULL && curr_idx < (idx - 1))
 	{
+		current = current->next;
+		curr_idx++;
 		if (current == NULL)
 		{
 			return (NULL);
-		}
-		current = current->next;
-		if (current->next == NULL)
-		{
-			next = NULL;
-		}
-		else
-		{
-			next = current->next;
 		}
 	}
 	new = malloc(sizeof(listint_t));
@@ -47,7 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = next;
+	new->next = current->next;
 	current->next = new;
 
 	return (new);
