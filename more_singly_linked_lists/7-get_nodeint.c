bfb9440087caf0c0aa6a1b8a5e0195ce31cdc70a
Davey <david.c.hays.iii@gmail.com>
2024-03-10 19:34:09 +0000

    small update

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current;
-	int ll_ind = 0;
+	unsigned int ll_ind = 0;
 
 	current = head;
 	while (ll_ind < index)
