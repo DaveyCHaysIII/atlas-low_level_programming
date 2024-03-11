72daa0cf123310162032ab48a0a0dfb408085264
Davey <david.c.hays.iii@gmail.com>
2024-03-11 04:39:37 +0000

    added file 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stddef.h>
+#include <sys/type.h>
+#include <stdlib.h>
+
+/**
+ * create_file()- create a file!
+ * @filename: name of file
+ * @text_content: content of text
+ *
+ * Return: int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, O_RDWR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	bytes_written = write(fd, text_content, _strlen(text_content));
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen()- length of string
+ * @str: string in question
+ *
+ * Return: lenght
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,8 @@
 #include <sys/types.h>
 
 int _putchar(char c);
+int _strlen(char *str);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
