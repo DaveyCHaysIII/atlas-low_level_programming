c1df84d1f5ffb6dc940714020c6722dd596feb38
Davey <david.c.hays.iii@gmail.com>
2024-03-11 05:17:50 +0000

    small update

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -26,14 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
-		if (errno == ENOENT)
-		{
-			return (-1);
-		}
-		else
-		{
-			return (-1);
-		}
+		return (-1);
 	}
 	if (text_content == NULL)
 	{
