066b63133c275b483380954a0bff879905defda1
Davey <david.c.hays.iii@gmail.com>
2024-03-11 05:05:26 +0000

    added file 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,61 @@
+#include "main.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * append_text_to_file()- appends text to a file
+ * @filename: filename
+ * @text_content: text content
+ *
+ * Return: int
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL | access(filename, F_OK | W_OK) != 0)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+	bytes_written(fd, text_content, _strlen(text_content));
+	if (bytes_written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen()- strlen
+ * @str: string
+ *
+ * Return: len
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
