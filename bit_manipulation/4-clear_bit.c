938ade854b76c90a29953b903c445f14270c42d4
Davey <david.c.hays.iii@gmail.com>
2024-03-11 02:28:47 +0000

    added file 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * clear_bit()- assigns a bit to 0
+ * @n: the number in question
+ * @index: the index to change
+ *
+ * Return: 1 on success, -1 if failed
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+
+	*n = (~(1UL << index) | *n);
+	return (1);
+}
