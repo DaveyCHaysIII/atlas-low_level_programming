bc4f5140200acbbc937c10e98d2071b023e39c09
Davey <david.c.hays.iii@gmail.com>
2024-03-11 01:47:52 +0000

    added file 1

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_binary()- prints a number to binary string
@@ -9,8 +11,38 @@
 
 void print_binary(unsigned long int n)
 {
+	int size, i, bit;
 
-	if (n > (1 >> j))
+	size = get_tens(n);
+	for (i = size - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+		{
+			putchar('1');
+		}
+		else
+		{
+			putchar('0');
+		}
+	}
+}
+
+/**
+ * get_tens()- figures how long a binary string is
+ * @n: the number in question
+ *
+ * Return: number of spaces big
+ */
+
+int get_tens(unsigned long int n)
+{
+	int j;
+
+	j = 1;
+	while ((int)n > (1 << j))
+	{
 		j++;
-
+	}
+	return (j);
 }
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -3,6 +3,8 @@
 
 int _putchar(char c);
 int _strlen(const char *c);
+void rev_string(char *str);
+int get_tens(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 
