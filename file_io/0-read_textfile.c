0711a068a773f4c6315e1fb76256e7866b590874
Davey <david.c.hays.iii@gmail.com>
2024-03-11 04:11:33 +0000

    small update

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * read_textfile()- reads a text file
@@ -13,14 +15,16 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
 	if (filename == NULL)
 	{
 		return (0);
 	}
 
-	int fd;
-	char buffer[letters];
-	ssize_t bytes_read, bytes_written;
+	buffer = malloc(letters);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -41,5 +45,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	close(fd);
+	free(buffer);
 	return (bytes_written);
 }
