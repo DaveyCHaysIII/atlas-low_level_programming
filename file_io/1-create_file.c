aa58713e0ec0b1a75cb4f9188418c1c9bbfdb9fd
Davey <david.c.hays.iii@gmail.com>
2024-03-11 04:45:17 +0000

    small update

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_written;
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		return (-1);
