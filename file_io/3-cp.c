856c3c3a592bdc61d8cd2d4b9f686b842de4263e
Davey <david.c.hays.iii@gmail.com>
2024-03-12 02:52:41 +0000

    small update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
-	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd_dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
