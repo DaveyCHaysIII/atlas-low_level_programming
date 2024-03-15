fdf6b4398259e71cc6dc50093302c1013997fb63
Davey <david.c.hays.iii@gmail.com>
2024-03-15 01:34:06 +0000

    small update

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new_node->n = n;
 	new_node->next = *head;
-	head->prev = new_node;
 	new_node->prev = NULL;
+	*head->prev = new_node;
 	*head = new_node;
 	return (new_node);
 }
