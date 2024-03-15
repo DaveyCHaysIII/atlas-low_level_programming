c77fe5f987348eff03b9720779cf58eb73639b41
Davey <david.c.hays.iii@gmail.com>
2024-03-15 02:22:00 +0000

    small update

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-	new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		free(new);
