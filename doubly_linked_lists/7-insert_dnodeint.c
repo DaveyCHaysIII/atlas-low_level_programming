2dd1db96a900ee282ebdf353045f1af8ea19ee2d
Davey <david.c.hays.iii@gmail.com>
2024-03-15 03:26:05 +0000

    small update

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 	new->next = current->next;
-	new->prev = current;
+	new->prev = current->prev;
 	current->next = new;
 	return (new);
 }
