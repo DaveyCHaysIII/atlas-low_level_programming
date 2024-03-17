da621942ebc42b51977cbee3cf898bade917b2b5
Davey <david.c.hays.iii@gmail.com>
2024-03-17 03:02:24 +0000

    small update

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -21,23 +21,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = (hash_djb2((const unsigned char *)key) % ht->size);
 	if (ht->array[index] != NULL)
 	{
-		if (strcmp(ht->array[index]->key, key) == 0)
-			return (ht->array[index]->value);
-		else if (ht->array[index]->next != NULL)
-		{
-			hash_node_t *current;
+		hash_node_t *current;
 
-			while (current->next != NULL)
+		current = ht->array[index];
+		while (current->next != NULL)
+		{
+			if (strcmp(current->key, key) != 0)
 			{
-				current = ht->array[index]->next;
-				if (strcmp(current->key, key) == 0)
-				{
-					return (current->value);
-				}
+				current = current->next;
 			}
+			else
+				return (current->value);
 		}
-		else
-			return (NULL);
+		return (NULL);
 	}
 	else
 		return (NULL);
