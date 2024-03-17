5b8e070f46dd13dabca009acd455e410b4c748b4
Davey <david.c.hays.iii@gmail.com>
2024-03-17 03:14:38 +0000

    small update

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,10 +14,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
-	{
-		return (NULL);
-	}
 	index = (hash_djb2((const unsigned char *)key) % ht->size);
 	if (ht->array[index] != NULL)
 	{
