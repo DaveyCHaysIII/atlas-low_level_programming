08ceb81ec93d771c9eb6780829713fe0e45c4200
Davey <david.c.hays.iii@gmail.com>
2024-03-17 01:46:16 +0000

    small update

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -36,7 +36,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		hash_node_t *current = ht->array[index];
-
+		
+		i = 0;
 		while (current->next != NULL)
 		{
 			current = current->next;
