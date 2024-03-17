6a73cd09b31aec7deaa316db9decca214a87eb8c
Davey <david.c.hays.iii@gmail.com>
2024-03-17 00:56:21 +0000

    added file 2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * key_index()- get a key index
+ * @key: the key
+ * @size: the size of the array
+ *
+ * Return: index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+	
+	index = djb2(key);
+	size++;
+	return (index);
+}
+
