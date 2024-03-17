963fdc26ba135d2e76b11475d048e4ef40037566
Davey <david.c.hays.iii@gmail.com>
2024-03-17 03:23:07 +0000

    small update

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * hash_table_get()- retrieve a value from the table
@@ -14,13 +15,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = (hash_djb2((const unsigned char *)key) % ht->size);
 	if (ht->array[index] != NULL)
 	{
 		hash_node_t *current;
 
 		current = ht->array[index];
-		while (current->next != NULL)
+		while (current != NULL)
 		{
 			if (strcmp(current->key, key) == 0)
 			{
