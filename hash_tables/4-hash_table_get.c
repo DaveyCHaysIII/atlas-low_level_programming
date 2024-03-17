bed934a11990759b78c91bd916b500fa4d13d3c8
Davey <david.c.hays.iii@gmail.com>
2024-03-17 02:33:17 +0000

    added file 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_table.h"
+
+/**
+ * hash_table_get()- retrieve a value from the table
+ * @ht: the table
+ * @key: the key
+ *
+ * Return: the value
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (*ht == NULL || *key == NULL)
+	{
+		return (NULL);
+	}
+	index = hash_djb2((const unsigned char *)key % ht->size);
+	if (ht->array[index] != NULL)
+	{
+		return (ht->array[index]->value);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
