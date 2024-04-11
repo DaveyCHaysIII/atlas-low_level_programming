8658546307c1de987883f71826796bd473160bfe
Davey <david.c.hays.iii@gmail.com>
2024-04-11 01:41:13 +0000

    added file 0

---
+++ search_algorithms/0-linear_search.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+ * linear_search()- the linear search algorithm
+ * @array: the arrray to search through
+ * @size: the size of the array
+ * @value: the value we're matching
+ *
+ * Return: index that holds matching value or -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value Checked Array[%u] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
+		}
+		return (-1);
+	}
+	return (-1);
+}
