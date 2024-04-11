f67156c23c4dbaa69224dd0add9dc1c28fd10a2b
Davey <david.c.hays.iii@gmail.com>
2024-04-11 02:17:59 +0000

    added nullcheck

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 	left = 0;
 	right = size - 1;
 
-	while (left <= right)
+	while (left <= right && array != NULL)
 	{
 		mid = left + (right - left) / 2;
 		printf("Searching in array: ");
