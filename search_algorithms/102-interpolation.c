d3b3779fd8408daa74edf1a68c86a46773bb2cde
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-05-05 22:19:46 -0500

    setting up files

---
+++ search_algorithms/102-interpolation.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - search using the interpolation algorithm
+ * @array: the array to search
+ * @size: the size of the array
+ * @value: the value to search for
+ *
+ * Return: index or -1
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+
+}
---
+++ search_algorithms/103-exponential.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * exponential_search - search through and array exponentially
+ * @array: the array in question
+ * @size: the size of the array
+ * @value: the value we are searching for
+ *
+ * Return: index or -1
+ *
+ */
+
+int exponential_search(int *array, size_t size, int value)
+{
+
+}
