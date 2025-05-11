da151fbdbec29c13ef79aa4b5cd46b362364a551
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-05-11 12:31:26 -0500

    added interpolation

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -11,5 +11,30 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
+	unsigned int pos, low, high;
 
+	low = 0;
+	high = size - 1;
+
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		pos = low + (((high - low) / (array[high] - array[low])) *
+		     (value - array[low]));
+
+		if (pos > (unsigned int)size || pos > UINT_MAX)
+		{
+			printf("Value checked array[%d] is out of range\n",
+			       pos);
+			return (-1);
+		}
+		printf("Value checked array[%d] = [%d]\n",
+		       pos, array[pos]);
+		if (value == array[pos])
+			return (pos);
+		else if (value < array[pos])
+			high = pos - 1;
+		else
+			low = pos + 1;
+	}
+	return (-1);
 }
