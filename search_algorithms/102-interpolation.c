c96aebb6271d70a3a4ae1418ca3d888dfcdb44b3
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-05-11 13:11:29 -0500

    spelling

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -33,7 +33,6 @@ int interpolation_search(int *array, size_t size, int value)
 		}
 		pos = low + (((double)(high - low) /
 		      (array[high] - array[low])) * (value - array[low]));
-
 		if (pos > (unsigned int)size ||
 		    pos > UINT_MAX ||
 		    pos == 49970605)
